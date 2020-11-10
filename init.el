eb19a7d971f89e7036731d0d1d7063ef7d18683d
Josh Skidmore <josh@josh.sc>
2020-11-10 12:06:00 -0500

    added json/jq support

--- init.el
+++ init.el
@@ -103,6 +103,7 @@
        ;;idris             ;
        (java +meghanada) ; the poster child for carpal tunnel syndrome
        javascript        ; all(hope(abandon(ye(who(enter(here))))))
+       json
        julia             ; a better, faster MATLAB
        ;;latex             ; writing papers in Emacs has never been so fun
        ;;ledger            ; an accounting system in Emacs
